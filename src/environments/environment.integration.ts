511255c2b62955778435f20c57202569d0ae4e81
aymen chaabani <caymen065@gmail.com>
2026-03-26 20:45:28 +0100

    edit env

--- src/environments/environment.integration.ts
+++ src/environments/environment.integration.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
   baseApiUrl: 'https://integration.yocoach.co',
-  keycloakUrl: 'https://login-int.yogym.co',
+  keycloakUrl: 'https://login-int.yogym.co/yo-coach-app',
+  apiUrl: 'https://integration.yocoach.co',
+  documentServiceUrl: 'http://https://integration.yocoach.co/document-management',
 };
